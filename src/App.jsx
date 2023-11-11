d6b5680d6ca208f0e915586d7a0f7456a90fd73b
anvar-muzaffarli <prof.anvar2013@gmail.com>
2023-11-11 20:40:25 +0400

    Layihe basladi ve Navbar yaradildi

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import NavMenyu from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <NavMenyu />
+
+    </>
+   
+    
+  
+  )
+}
+
+export default App
---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../assets/css/Button.css'
+
+function Button() {
+  return (
+    <button className='shexsi-btn'>Button</button>
+  )
+}
+
+export default Button
